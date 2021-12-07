4113086f38aa6f58b578a49a8f37903a8d20bb76
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-12-07 04:16:38 +0000

    Update All File's

---
+++ ThorExMeme.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts-upgradeable@4.4.0/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.4.0/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.4.0/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.4.0/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.4.0/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.4.0/token/ERC20/extensions/ERC20FlashMintUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.4.0/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable@4.4.0/proxy/utils/UUPSUpgradeable.sol";
+
+contract ThorExMeme is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable, PausableUpgradeable, ERC20FlashMintUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer public {
+        __ERC20_init("ThorExMeme", "THEM");
+        __ERC20Burnable_init();
+        __ERC20Snapshot_init();
+        __Ownable_init();
+        __Pausable_init();
+        __ERC20FlashMint_init();
+        __UUPSUpgradeable_init();
+
+        _mint(msg.sender, 2100000000000000000000000000000000000000 * 10 ** decimals());
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
